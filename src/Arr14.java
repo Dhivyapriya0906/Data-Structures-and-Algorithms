1efe3fea6f4102e5bf5d2bb798ef3b2341796781
Dhivyapriya0906 <indiradhivyapriya@gmail.com>
2026-04-09 06:32:57 +0530

    DSA ARRAY Easy Problems

--- src/Arr14.java
+++ src/Arr14.java
@@ -1,3 +1,21 @@
 public class Arr14 {
-
+public static void main(String[] args){
+    //positive == negative
+    int[] arr=new int[]{7,5,-1,8,-2,-65};
+    int neg=1 , pos=0;
+    int[] ans=new int[arr.length];
+    for(int i=0;i< arr.length;i++){
+        if(arr[i]<0){
+            ans[neg]=arr[i];
+            neg+=2;
+        }
+        else{
+            ans[pos]=arr[i];
+            pos+=2;
+        }
+    }
+    for(int x: ans){
+        System.out.print(x+" ");
+    }
+}
 }
