1c1204c771f5edd88c9aa1ac4648b0be6b6fa143
Dhivyapriya0906 <indiradhivyapriya@gmail.com>
2026-04-09 21:49:15 +0530

    DSA ARRAY Medium Problems

--- src/Arr15_1.java
+++ src/Arr15_1.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Arr15_1 {
     public static void main(String[] args){
-        int[] arr=new int[]{7,5,-1,8,-2,-65};
+        int[] arr=new int[]{16, 17, 4, 3 ,5 ,2};
         ArrayList<Integer> a=new ArrayList<>();
         int max=arr[arr.length-1];
-        for(int i= arr.length-1;i>=0;i--){
+        a.add(max);
+        for(int i= arr.length-2;i>=0;i--){
             if(max<arr[i]){
                 max=arr[i];
                 a.add(arr[i]);
 
             }
         }
+        Collections.reverse(a);
         System.out.println(a);
     }
 }
